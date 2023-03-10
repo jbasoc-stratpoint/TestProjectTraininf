cd6608e6a9b4205792ac95d48913bead90df46d9
Jan Basoc <jbasoc@stratpoint.com>
2023-03-10 09:21:08 +0000

    feat: adjust primaryLayout function

--- my-t3-app/src/components/layouts/primary/PrimaryLayout.tsx
+++ my-t3-app/src/components/layouts/primary/PrimaryLayout.tsx
@@ -5,13 +5,13 @@ import styles from './PrimaryLayout.module.css';
 
 export interface IPrimaryLayout {}
 
-const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({}) => {
   return (
     <>
       <Head>
         <title> Primary Layout Example</title>
       </Head>
-      <main className={styles.main}>{children}</main>
+      <main className={styles.main}>{}</main>
     </>
   );
 };
